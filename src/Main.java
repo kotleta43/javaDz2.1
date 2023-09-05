1c10e901882aa77e1f00cb729db63aa53b47aa6b
Zeziulin Sergey <kotleta353@gmail.com>
2023-09-05 20:14:40 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 11559;
+        int minimum = 20;
+        int bonus = price / minimum;
+
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
